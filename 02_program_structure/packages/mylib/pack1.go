ed7e73108fb8aaafa89c5636fad9f0fc6975af34
crrrrrw <120755274@qq.com>
2019-09-03 21:52:26 +0800

    目录重构

---
+++ 02_program_structure/packages/mylib/pack1.go
@@ -0,0 +1,9 @@
+package pack
+
+type Price float64
+type Count int64
+
+const (
+	Pen        Price = 10.5
+	TotalCount Count = 15
+)
---
+++ 02_program_structure/packages/mylib/pack2.go
@@ -0,0 +1,5 @@
+package pack
+
+func Multy(p float64, cnt float64) float64 {
+	return p * cnt
+}
